52f5dac9d87f54bc21f4961e59e6076ce4869bdb
Micael Rodrigues <micaelr95@outlook.com>
2019-11-20 12:57:36 +0000

    Change database schema

--- techpower.sql
+++ techpower.sql
@@ -1,77 +1,127 @@
-CREATE DATABASE tech_power;
-USE tech_power;
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
 
 
--- ********* Tabela Utilizadores ********* --
-CREATE TABLE `user`(
- `id`                   int(11) NOT NULL PRIMARY KEY,
- `username`             varchar(255) NOT NULL ,
- `auth_key`             varchar(32) NOT NULL ,
- `password_hash`        varchar(255) NOT NULL ,
- `password_reset_token` varchar(255) NOT NULL ,
- `email`                varchar(255) NOT NULL ,
- `status`               int(6) NOT NULL ,
- `created_at`           int(11) NOT NULL ,
- `updated_at`           int(11) NOT NULL ,
- `verification_token`   varchar(255) NOT NULL
-) ENGINE = InnoDb;
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `techpower`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `category`
+--
+
+DROP TABLE IF EXISTS `category`;
+CREATE TABLE IF NOT EXISTS `category` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `description` varchar(200) NOT NULL,
+  `parent_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `parent_id` (`parent_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+
+--
+-- Estrutura da tabela `product`
+--
+
+DROP TABLE IF EXISTS `product`;
+CREATE TABLE IF NOT EXISTS `product` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `product_name` varchar(50) NOT NULL,
+  `unit_price` decimal(12,2) DEFAULT NULL,
+  `is_discontinued` bit(1) NOT NULL DEFAULT b'0',
+  `description` varchar(5000) NOT NULL,
+  `id_category` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `id_category` (`id_category`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
 
 
--- ********* Tabela Perfis ********* --
-CREATE TABLE `profile`(
- `phone`       varchar(20) NULL ,
- `address`     varchar(255) NULL ,
- `nif`         int(9) NULL ,
- `postal_code` varchar(8) NULL ,
- `city`        varchar(50) NULL ,
- `country`     varchar(100) NULL ,
- `id`          int(11) NOT NULL,
-FOREIGN KEY (`id`) REFERENCES `user` (`id`)
-) ENGINE = InnoDb;
+--
+-- Estrutura da tabela `profile`
+--
 
+DROP TABLE IF EXISTS `profile`;
+CREATE TABLE IF NOT EXISTS `profile` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `phone` varchar(20) DEFAULT NULL,
+  `address` varchar(255) DEFAULT NULL,
+  `nif` int(9) DEFAULT NULL,
+  `postal_code` varchar(8) DEFAULT NULL,
+  `city` varchar(50) DEFAULT NULL,
+  `country` varchar(100) DEFAULT NULL,
+  `id_user` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `id` (`id_user`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- ********* Tabela Categorias ********* --
-CREATE TABLE `category`(
- `id`          int NOT NULL PRIMARY KEY AUTO_INCREMENT,
- `description` varchar(200) NOT NULL ,
- `parent_id`   int,
-FOREIGN KEY (`parent_id`) REFERENCES `category` (`id`)
-) ENGINE = InnoDb;
+-- --------------------------------------------------------
 
--- ********* Tabela Vendas ********* --
-CREATE TABLE `sale`(
- `id`            int NOT NULL AUTO_INCREMENT PRIMARY KEY,
- `sale_date`     datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ,
- `total_amount`  decimal(12,2) NOT NULL ,
- `sale_finished` bit NOT NULL
-) ENGINE = InnoDb;
+--
+-- Estrutura da tabela `sale`
+--
 
+DROP TABLE IF EXISTS `sale`;
+CREATE TABLE IF NOT EXISTS `sale` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `sale_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `total_amount` decimal(12,2) NOT NULL,
+  `sale_finished` bit(1) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- ********* Tabela Produtos ********* --
-CREATE TABLE `product`(
- `id`              int NOT NULL AUTO_INCREMENT PRIMARY KEY,
- `product_name`    varchar(50) NOT NULL ,
- `unit_price`      decimal(12,2) NULL ,
- `is_discontinued` bit NOT NULL DEFAULT 0 ,
- `description`     varchar(5000) NOT NULL ,
- `id_category`     int NOT NULL ,
-FOREIGN KEY (`id_category`) REFERENCES `category` (`id`)
-) ENGINE = InnoDB;
+-- --------------------------------------------------------
 
+--
+-- Estrutura da tabela `sale_item`
+--
 
--- ********* Tabela Linha de Vendas ********* --
-CREATE TABLE `sale_item`(
- `id`         int NOT NULL PRIMARY KEY,
- `unit_price` decimal(12,2) NOT NULL ,
- `quantity`   int NOT NULL ,
- `id_product` int NOT NULL ,
- `id_sale`    int NOT NULL ,
-FOREIGN KEY (`id_product`) REFERENCES `product` (`id`),
-FOREIGN KEY (`id_sale`) REFERENCES `sale` (`id`)
-) ENGINE = InnoDB;
+DROP TABLE IF EXISTS `sale_item`;
+CREATE TABLE IF NOT EXISTS `sale_item` (
+  `id` int(11) NOT NULL,
+  `unit_price` decimal(12,2) NOT NULL,
+  `quantity` int(11) NOT NULL,
+  `id_product` int(11) NOT NULL,
+  `id_sale` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `id_product` (`id_product`),
+  KEY `id_sale` (`id_sale`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Limitadores para a tabela `category`
+--
+ALTER TABLE `category`
+  ADD CONSTRAINT `category_ibfk_1` FOREIGN KEY (`parent_id`) REFERENCES `category` (`id`);
 
+--
+-- Limitadores para a tabela `product`
+--
+ALTER TABLE `product`
+  ADD CONSTRAINT `product_ibfk_1` FOREIGN KEY (`id_category`) REFERENCES `category` (`id`);
 
+--
+-- Limitadores para a tabela `profile`
+--
+ALTER TABLE `profile`
+  ADD CONSTRAINT `profile_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `user` (`id`);
 
+--
+-- Limitadores para a tabela `sale_item`
+--
+ALTER TABLE `sale_item`
+  ADD CONSTRAINT `sale_item_ibfk_1` FOREIGN KEY (`id_product`) REFERENCES `product` (`id`),
+  ADD CONSTRAINT `sale_item_ibfk_2` FOREIGN KEY (`id_sale`) REFERENCES `sale` (`id`);
+COMMIT;
 
-
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;

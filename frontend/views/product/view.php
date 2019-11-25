cf8f5506a5af64af595fcb456b6d24e0c9f36317
micaelr95 <micaelr95@outlook.pt>
2019-11-25 19:57:17 +0000

    Add style to product details page

--- frontend/views/product/view.php
+++ frontend/views/product/view.php
@@ -9,12 +9,16 @@ $this->title = $model->product_name;
 ?>
 <div class="product-view">
 
-    <h3><?= Html::encode($model->product_name) ?></h3>
-
-    <img src="https://d3ift91kaax4b9.cloudfront.net/media/catalog/product/cache/33ba37c1fda6d70c703e88ff79ea1021/p/r/product-p006949-517_1.jpg" class="card-img-top" alt="...">
-
+    <h2 class="product-name"><?= Html::encode($model->product_name) ?></h2>
+    
+    <img class="product-image col-md-6 col-lg-5" src="https://d3ift91kaax4b9.cloudfront.net/media/catalog/product/cache/33ba37c1fda6d70c703e88ff79ea1021/p/r/product-p006949-517_1.jpg" alt="...">
+    
+    <h4>Descrição</h4>
     <p><?= Html::encode($model->description) ?></p>
-
-    <p><?= Html::encode($model->unit_price) ?>€</p>
+    
+    <div class="product-bottom">
+        <h4 class="product-price"><?= Html::encode($model->unit_price) ?>€</h4>
+        <?= Html::a('Add to cart', ['create'], ['class' => 'btn btn-success']) ?>
+    </div>
 
 </div>

de44f16555532713a3ecd1c4b9726ad0446f9802
MartimTSilva <43602260+MartimTSilva@users.noreply.github.com>
2019-12-01 16:29:42 +0000

    Show order details

--- frontend/views/sale/view.php
+++ frontend/views/sale/view.php
@@ -2,21 +2,26 @@
 
 use yii\helpers\Html;
 use yii\widgets\DetailView;
+use common\models\Product;
 
 /* @var $this yii\web\View */
 /* @var $model common\models\Sale */
+/* @var $sale_item_model common\models\Sale */
+/* @var $profile common\models\Sale */
+
 
 $this->title = 'Encomenda #' . $model->id . ' | TechPower';
 ?>
+
 <div class="sale-view">
     <h1>Encomenda #<?= $model->id ?></h1>
     <br>
     <h4 style="font-weight: bold;">Morada de entrega</h4>
-    <p>John Doe</p>
-    <p>Rua Professor piedade Arinto, lote nº 1</p>
-    <p>Leiria</p>
-    <p>2120-500</p>
-    <p>Portugal</p>
+    <p>TODO ---------> add column name to database table profile</p>
+    <p><?= $profile['address']?></p>
+    <p><?= $profile['city']?></p>
+    <p><?= $profile['postal_code']?></p>
+    <p><?= $profile['country']?></p>
     <hr>
     <h4 style="font-weight: bold;">Resumo de compra</h4>
     <table class="table">
@@ -30,27 +35,33 @@ $this->title = 'Encomenda #' . $model->id . ' | TechPower';
             </tr>
         </thead>
         <tbody>
+        <?php foreach($sale_item_model as $item){ 
+            // **Needs refactoring** Procura os dados do produto através do id_product na tabela sale_item
+            $product_id = $item['id_product'];
+            $product_info = Product::find()->where(['id' => $product_id])->one();  ?>
+
             <tr>
                 <td data-th="Item">
                     <div class="row">
                         <div class="col-sm-2 hidden-xs"><img src="https://d3ift91kaax4b9.cloudfront.net/media/catalog/product/cache/33ba37c1fda6d70c703e88ff79ea1021/p/r/product-p015531-39574_1.jpg" alt="..." class="img-responsive"/></div>
                         <div class="col-sm-9">
-                            <h5>Processador Intel Core i9-9900KS Octa-Core 4.0GHz c/ Turbo 5.0GHz 16MB Skt1151</h5>
+                            <h5><?= $product_info['product_name'] ?></h5>
                         </div>
                     </div>
                 </td>
-                <td data-th="Preço" class="text-center">596,90€</td>
-                <td data-th="Quantidade" class="text-center">1</td>           
-                <td data-th="Subtotal" class="text-center">596,90€</td>           
+                <td data-th="Preço" class="text-center"><?= $item['unit_price'] ?>€</td>
+                <td data-th="Quantidade" class="text-center"><?= $item['quantity'] ?></td>           
+                <td data-th="Subtotal" class="text-center"><?= $item['unit_price'] * $item['quantity'] ?>€</td>           
             </tr>
+        <?php } ?>
         </tbody>
         <tfoot>
             <tr class="visible-xs">
-                <td class="text-center"><strong>Total: 596,90€</strong></td>
+                <td class="text-center"><strong>Total: <?= $model->total_amount?>€</strong></td>
             </tr>
             <tr>
                 <td colspan="3" class="hidden-xs"></td>
-                <td class="hidden-xs text-center"><strong>Total 596,90€</strong></td>
+                <td class="hidden-xs text-center"><strong>Total: <?= $model->total_amount?>€</strong></td>
             </tr>
         </tfoot>
     </table>

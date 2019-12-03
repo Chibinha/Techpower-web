288237892c8b1a447e12043191bb219a79a7c28c
Micael Rodrigues <micaelr95@outlook.com>
2019-12-03 09:34:45 +0000

    Add searchbox to navbar and add new navbar breakpoint

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -16,6 +16,7 @@ AppAsset::register($this);
 <?php $this->beginPage() ?>
 <!DOCTYPE html>
 <html lang="<?= Yii::$app->language ?>">
+
 <head>
     <meta charset="<?= Yii::$app->charset ?>">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -24,84 +25,89 @@ AppAsset::register($this);
     <title><?= Html::encode($this->title) ?></title>
     <?php $this->head() ?>
 </head>
+
 <body>
-<?php $this->beginBody() ?>
+    <?php $this->beginBody() ?>
 
-<div class="wrap">
-    <?php
-    NavBar::begin([
-        'brandLabel' => Yii::$app->name,
-        'brandUrl' => Yii::$app->homeUrl,
-        'options' => [
-            'class' => 'navbar-inverse navbar-fixed-top',
-        ],
-    ]);
-
-    ?>
-    <form class="navbar-form navbar-left" action="/action_page.php">
-        <div class="form-group has-feedback search">
-            <input type="text" class="form-control" placeholder="Procurar..." />
-            <i class="glyphicon glyphicon-search form-control-feedback"></i>
-        </div>
-    </form>   
-    <?php
-
-    $menuItems = [        
-        ['label' => '<span class="glyphicon glyphicon-home"></span> &ensp; Página Inicial', 'url' => ['/site/index']],
-        ['label' => '<span class="glyphicon glyphicon-align-justify"></span> &ensp; Categorias',
-        'items' => Helper::getCategories(),
-    ],
-        ['label' => '<span class="glyphicon glyphicon-shopping-cart"></span> &ensp; Carrinho', 'url' => ['/site/cart']],
-    ];
-    if (Yii::$app->user->isGuest) {
-        $menuItems[] = ['label' => 'Registar', 'url' => ['/site/signup']];
-        $menuItems[] = ['label' => 'Login', 'url' => ['/site/login']];
-    } else {
-        $menuItems[] = [
-            'label' => '<span class="glyphicon glyphicon-user"></span> &ensp; A minha conta',
-            'items' => [
-                [
-                    'label' => '<span class="glyphicon glyphicon-info-sign"></span> &ensp; Informações da Conta',
-                    'url' => ['/user/update', 'id' => Helper::getLoggedUser()],
-                ],
-                [
-                    'label' => '<span class="glyphicon glyphicon-gift"></span> &ensp; Encomendas',
-                    'url' => ['/sale/index', 'id' => Helper::getLoggedUser()],
-                ],
-                [
-                    'label' => '<span class="glyphicon glyphicon-remove"></span> &ensp; Logout',
-                    'url' => ['/site/logout'],
-                    'linkOptions' => ['data-method' => 'post']
-                ],      
+    <div class="wrap">
+        <?php
+        NavBar::begin([
+            'brandLabel' => Yii::$app->name,
+            'brandUrl' => Yii::$app->homeUrl,
+            'options' => [
+                'class' => 'navbar-inverse navbar-fixed-top',
             ],
+        ]);
+
+        $menuItems = [
+            ['label' => '<li>'
+                . Html::beginForm(['/site/search'], 'post', ['class' => 'navbar-form'])
+                . '<div class="form-group has-feedback search">'
+                . Html::textInput('query', 'Procurar...', ['class' => 'form-control'])
+                . Html::submitButton(
+                    '',
+                    ['class' => 'glyphicon glyphicon-search form-control-feedback']
+                )
+                . '</div>'
+                . Html::endForm()
+                . '</li>'],
+            ['label' => '<span class="glyphicon glyphicon-home"></span> &ensp; Página Inicial', 'url' => ['/site/index']],
+            [
+                'label' => '<span class="glyphicon glyphicon-align-justify"></span> &ensp; Categorias',
+                'items' => Helper::getCategories(),
+            ],
+            ['label' => '<span class="glyphicon glyphicon-shopping-cart"></span> &ensp; Carrinho', 'url' => ['/site/cart']],
         ];
-    }
-    echo Nav::widget([
-        'options' => ['class' => 'navbar-nav navbar-right'],
-        'encodeLabels' => false,
-        'items' => $menuItems,
-    ]);
-    NavBar::end();
-    ?>
+        if (Yii::$app->user->isGuest) {
+            $menuItems[] = ['label' => 'Registar', 'url' => ['/site/signup']];
+            $menuItems[] = ['label' => 'Login', 'url' => ['/site/login']];
+        } else {
+            $menuItems[] = [
+                'label' => '<span class="glyphicon glyphicon-user"></span> &ensp; A minha conta',
+                'items' => [
+                    [
+                        'label' => '<span class="glyphicon glyphicon-info-sign"></span> &ensp; Informações da Conta',
+                        'url' => ['/user/update', 'id' => Helper::getLoggedUser()],
+                    ],
+                    [
+                        'label' => '<span class="glyphicon glyphicon-gift"></span> &ensp; Encomendas',
+                        'url' => ['/sale/index', 'id' => Helper::getLoggedUser()],
+                    ],
+                    [
+                        'label' => '<span class="glyphicon glyphicon-remove"></span> &ensp; Logout',
+                        'url' => ['/site/logout'],
+                        'linkOptions' => ['data-method' => 'post']
+                    ],
+                ],
+            ];
+        }
+        echo Nav::widget([
+            'options' => ['class' => 'navbar-nav navbar-right col-'],
+            'encodeLabels' => false,
+            'items' => $menuItems,
+        ]);
+        NavBar::end();
+        ?>
 
-    <div class="container">
-        <?= Breadcrumbs::widget([
-            'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
-        ]) ?>
-        <?= Alert::widget() ?>
-        <?= $content ?>
+        <div class="container">
+            <?= Breadcrumbs::widget([
+                'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
+            ]) ?>
+            <?= Alert::widget() ?>
+            <?= $content ?>
+        </div>
     </div>
-</div>
 
-<footer class="footer">
-    <div class="container">
-        <p class="pull-left">&copy; <?= Html::encode(Yii::$app->name) ?> <?= date('Y') ?></p>
+    <footer class="footer">
+        <div class="container">
+            <p class="pull-left">&copy; <?= Html::encode(Yii::$app->name) ?> <?= date('Y') ?></p>
 
-        <p class="pull-right"><?= Yii::powered() ?></p>
-    </div>
-</footer>
+            <p class="pull-right"><?= Yii::powered() ?></p>
+        </div>
+    </footer>
 
-<?php $this->endBody() ?>
+    <?php $this->endBody() ?>
 </body>
+
 </html>
 <?php $this->endPage() ?>

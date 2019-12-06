2bd77d756c49cdac54f40fd1dd562dec572f8a2a
Martim Silva <martimtsilva@users.noreply.github.com>
2019-12-06 11:22:18 +0000

    Fix card styles

--- frontend/views/category/view.php
+++ frontend/views/category/view.php
@@ -23,8 +23,8 @@ $this->title = $model->description . ' | TechPower';
                             <a href="<?=Url::to(['product/view', 'id' => $product["id"]]); ?>">
                             <img src="https://d3ift91kaax4b9.cloudfront.net/media/catalog/product/cache/33ba37c1fda6d70c703e88ff79ea1021/p/r/product-p006949-517_1.jpg" class="card-img-top" alt="...">
                             <div class="card-body">
-                                <h5 class="card-title"><?= StringHelper::truncate(Html::encode($product['product_name']), 42) ?></h5>
-                                <p class="card-text description"><?= StringHelper::truncate(Html::encode(($product['description'])), 58)?></p>
+                                <h5 class="card-title"><?= $product['product_name'] ?></h5>
+                                <p class="card-text description"><?= $product['description']?></p>
                                 <p class="card-text price"><?= $product['unit_price'] ?>€</p>
                             </div>
                             </a>

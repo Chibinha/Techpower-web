5a5c335d1b2ff105a8878f6dc9c9c1e2fb6781fa
MartimTSilva <43602260+MartimTSilva@users.noreply.github.com>
2019-12-07 19:34:23 +0000

    Http Basic Auth

--- backend/api/controllers/DefaultController.php
+++ backend/api/controllers/DefaultController.php
@@ -2,7 +2,10 @@
 
 namespace app\api\controllers;
 
+use common\models\User;
+use yii\debug\models\search\User as SearchUser;
 use yii\web\Controller;
+use yii\filters\auth\HttpBasicAuth;
 
 /**
  * Default controller for the `api` module
@@ -17,4 +20,23 @@ class DefaultController extends Controller
     {
         return $this->render('index');
     }
+
+    public function behaviors()
+    {
+        $behaviors = parent::behaviors();
+        $behaviors['authenticator'] = [
+            'class' => HttpBasicAuth::className(),
+            'auth' => function ($username, $password)
+            {
+                $user = User::findByUsername($username);
+                if ($user && $user->validatePassword($password))
+                {
+                    return $user;
+                }
+            }
+        ];
+        return $behaviors;      
+    }
+
+
 }

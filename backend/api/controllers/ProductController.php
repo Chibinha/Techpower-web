91cce867b2b52df5fc75a6dcc0af1df978023407
Chibinha <47814243+Chibinha@users.noreply.github.com>
2019-12-13 12:03:42 +0000

    Update API get products by name

--- backend/api/controllers/ProductController.php
+++ backend/api/controllers/ProductController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace app\api\controllers;
+use common\models\Product;
 
 use Yii;
 use yii\rest\ActiveController;
@@ -8,4 +9,11 @@ use yii\rest\ActiveController;
 class ProductController extends ActiveController
 {
     public $modelClass = 'common\models\Product';
+
+    //http://localhost:8081/api/products/{name}
+    public function actionProductsByName($id){
+
+        $productsmodel = Product::find()->where("product_name=".)->all();
+        return ['products' => $productsmodel];
+    }
 }

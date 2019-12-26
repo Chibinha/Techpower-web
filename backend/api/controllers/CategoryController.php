9c96ed430a5d3a9a178665c2e19b894c7032a8ba
micaelr95 <micaelr95@outlook.pt>
2019-12-26 00:53:50 +0000

    Add authorization to category and product controllers

--- backend/api/controllers/CategoryController.php
+++ backend/api/controllers/CategoryController.php
@@ -5,10 +5,31 @@ namespace app\api\controllers;
 use Yii;
 use yii\rest\ActiveController;
 use common\models\Product;
+use yii\filters\auth\QueryParamAuth;
 
 class CategoryController extends ActiveController
 {
     public $modelClass = 'common\models\Category';
+
+    public function behaviors()
+    {
+        $behaviors = parent::behaviors();
+        $behaviors['authenticator'] = [
+            'class' => QueryParamAuth::className(),
+            'except' => ['index', 'view'],
+        ];
+        return $behaviors;
+    }
+
+    public function checkAccess($action, $model = null, $params = [])
+    {
+        // check if the user can access $action and $model
+        // throw ForbiddenHttpException if access should be denied
+        if ($action === 'create' || $action === 'update' || $action === 'delete') {
+            if(\Yii::$app->user->isGuest)
+                throw new \yii\web\ForbiddenHttpException('Your request was made with invalid credentials.');
+        }
+    }
 
     //http://localhost:8080/api/categories/products/{id}
     public function actionProducts($id)
--- backend/api/controllers/ProductController.php
+++ backend/api/controllers/ProductController.php
@@ -2,6 +2,7 @@
 
 namespace app\api\controllers;
 use common\models\Product;
+use yii\filters\auth\QueryParamAuth;
 
 use Yii;
 use yii\rest\ActiveController;
@@ -9,6 +10,26 @@ use yii\rest\ActiveController;
 class ProductController extends ActiveController
 {
     public $modelClass = 'common\models\Product';
+
+    public function behaviors()
+    {
+        $behaviors = parent::behaviors();
+        $behaviors['authenticator'] = [
+            'class' => QueryParamAuth::className(),
+            'except' => ['index', 'view'],
+        ];
+        return $behaviors;
+    }
+
+    public function checkAccess($action, $model = null, $params = [])
+    {
+        // check if the user can access $action and $model
+        // throw ForbiddenHttpException if access should be denied
+        if ($action === 'create' || $action === 'update' || $action === 'delete') {
+            if(\Yii::$app->user->isGuest)
+                throw new \yii\web\ForbiddenHttpException('Your request was made with invalid credentials.');
+        }
+    }
 
     //http://localhost:8081/api/products/name/{name}
     public function actionTeste($name)

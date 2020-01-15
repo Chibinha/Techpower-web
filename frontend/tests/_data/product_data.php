0a34590439119c4d2a2e90a63de1057f8a490ef0
MartimTSilva <43602260+MartimTSilva@users.noreply.github.com>
2020-01-15 00:45:41 +0000

    Add functional test frontend user update

---
+++ frontend/tests/_data/product_data.php
@@ -0,0 +1,10 @@
+<?php
+return [
+    [
+        'product_name' => 'Category',
+        'unit_price' => 1,
+        'is_discontinued' => 0,
+        'description' => 'description',
+        'id_category' => 1,
+    ],
+];
